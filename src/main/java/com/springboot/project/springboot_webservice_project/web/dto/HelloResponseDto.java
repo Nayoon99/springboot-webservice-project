cc92977c16c08505f2542a8fa690b20c3ae901f9
Nayoon99 <kny99306ny@gmail.com>
2024-10-01 04:27:27 +0900

    스프링 부트에서 테스트 코드 작성하기

---
+++ src/main/java/com/springboot/project/springboot_webservice_project/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.springboot.project.springboot_webservice_project.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
