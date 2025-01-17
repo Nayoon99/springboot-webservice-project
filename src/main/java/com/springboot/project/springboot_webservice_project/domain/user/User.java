fd56f9d374f2cb2192b1623a7cde2e0c6fc5e456
Nayoon99 <kny99306ny@gmail.com>
2025-01-18 08:33:13 +0900

    Feat: 사용자 정보 담당 user 클래스 생성

---
+++ src/main/java/com/springboot/project/springboot_webservice_project/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.springboot.project.springboot_webservice_project.domain.user;
+
+import com.springboot.project.springboot_webservice_project.domain.posts.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
